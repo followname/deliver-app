5c5482afcaab2db3cae89bb4b63dba20b3b37c47
followname <merkonjb@gmail.com>
2019-12-19 21:56:23 +0300

    auth/register front with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import AppNavbar from "./components/AppNavbar";
 
-function App() {
-  return <div>hi</div>;
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+        </div>
+        <div></div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
